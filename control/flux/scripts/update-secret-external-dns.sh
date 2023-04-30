306c3f0381ad502ba5ebb36f0edcfb47d279785a
haggishunk <travis@mattera.io>
2023-04-30 09:09:43 -0700

    Added scripts for helpful secrets management. Updated externaldns do secret.

---
+++ control/flux/scripts/update-secret-external-dns.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -feuo pipefail
+
+SECRET_NAME=digitalocean-token
+SECRET_KEY=digitalocean_api_token
+DESTINATION_DIR=./infrastructure/clusters/gorgeous/external-dns
+
+echo $DIGITALOCEAN_TOKEN_EXTERNAL_DNS \
+    | kubectl create secret generic \
+        $SECRET_NAME \
+        -n default \
+        --from-file=$SECRET_KEY=/dev/stdin \
+        -oyaml \
+        --dry-run=client > $DESTINATION_DIR/sops-secret.yaml
+
+sops \
+    --hc-vault-transit $VAULT_ADDR/v1/sops/keys/firstkey \
+    --encrypt \
+    --encrypted-regex '^(data|stringData)$' \
+    --in-place $DESTINATION_DIR/sops-secret.yaml
